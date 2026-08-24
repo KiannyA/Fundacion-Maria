130e70283762016ab61f52dbf92277ae0046c3fc
KiannyA <kiannyarguedas05@gmail.com>
2026-08-24 11:25:41 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+function mostrarMensaje() {
+
+    alert(
+        "La Fundación María nació de un acto de solidaridad y amor hacia las personas adultas mayores."
+    );
+
+    document.getElementById("historia").scrollIntoView({
+        behavior: "smooth"
+    });
+
+}
+
+
+function donar() {
+
+    alert(
+        "¡Gracias por tu interés en ayudar! Cada aporte contribuye al bienestar de nuestros adultos mayores."
+    );
+
+}
